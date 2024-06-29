5f0724fc75ef4af6d4588e31d033d76835909619
tequ <git@tequ.dev>
2024-06-29 17:58:17 +0900

    refact consumers

--- src/consumers/ledger/ledger.comsumer.ts
+++ src/consumers/ledger/ledger.comsumer.ts
@@ -5,20 +5,17 @@ import { LedgerConsumerValue } from 'src/types/consumers/ledger'
 
 @Injectable()
 export class LedgerConsumer extends KafkaConsumer {
-  readonly consumerGroupName: string
-  readonly consumerTopicName: string
+  readonly consumerGroupName = 'LedgerConsumer'
+  readonly consumerTopicName = 'ledger'
+
   constructor(private readonly dataStoreService: DataStoreService) {
     super()
-    this.consumerGroupName = 'LedgerConsumer'
-    this.consumerTopicName = 'ledger'
   }
+
   handler(key: string, value: LedgerConsumerValue): void {
     this.logger.log(`${value.ledger_index}: ${value.ledger_time_iso}`)
 
     const indexName = `ledger-${Math.trunc(value.ledger_index / 1_000_000)}m`
     this.dataStoreService.add(indexName, key, value)
-    /**
-     * Write Consumer-specific logic
-     */
   }
 }
--- src/consumers/transaction/transaction.comsumer.ts
+++ src/consumers/transaction/transaction.comsumer.ts
@@ -5,19 +5,16 @@ import { TransactionConsumerValue } from 'src/types/consumers/transaction'
 
 @Injectable()
 export class TransactionConsumer extends KafkaConsumer {
-  readonly consumerGroupName: string
-  readonly consumerTopicName: string
+  readonly consumerGroupName = 'TransactionConsumer'
+  readonly consumerTopicName = 'transaction'
+
   constructor(private readonly dataStoreService: DataStoreService) {
     super()
-    this.consumerGroupName = 'TransactionConsumer'
-    this.consumerTopicName = 'transaction'
   }
+
   handler(key: string, value: TransactionConsumerValue): void {
     this.logger.log(`${value.ledger_index}: ${key}`)
     const indexName = `transaction-${Math.trunc(value.ledger_index / 1_000_000)}m`
     this.dataStoreService.add(indexName, key, value)
-    /**
-     * Write Consumer-specific logic
-     */
   }
 }
